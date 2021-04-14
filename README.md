0303f8ab1996342552557e3d7bf25f391ee45c2a
cristal-node <12prottoy2002@gmail.com>
2021-04-14 16:25:00 +0600

    update

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Downloads all video and notes to specific directory. And can resume, by checking file.
 But file resume is not yet available
 
+Initialization: put your configuration in config.json like main brunch
 
 Usage:
 1. pip3 install -U -r requirements.txt
-2. Change the cookie and user-agent of your active login in the main.py file.
+2. Change the cookie and user-agent of your active login in the config.json file.
 3. python3 main.py #Don't use python2
--- main.py
+++ main.py
@@ -1,90 +1,12 @@
 #!/usr/bin/python
 
+import json
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-personal_cookie = ''
-user_agent = ''
-
-def cls_notes(link, pp):
-    rlink = "https://online.udvash-unmesh.com" + link
-    headers = {
-        'authority': 'online.udvash-unmesh.com',
-        'user-agent': user_agent,
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://online.udvash-unmesh.com/Routine',
-        'accept-language': 'en-US,en;q=0.9,ja;q=0.8',
-        'cookie': personal_cookie,
-        'sec-gpc': '1',
-    }
-    response = requests.get(rlink, headers=headers)
-    tsoup = BeautifulSoup(response.text, features='lxml')
-    name = " ".join(tsoup.find('div', attrs={'class': 'col-md-6'}).string.split()[2:])
-    url = tsoup.find('iframe').attrs['src'].split('#')[0]
-    file_name = pp + '/' + name + '.pdf'
-    if ( not Path(file_name).is_file() ) or ( Path(file_name).stat().st_size != int(requests.head(url, headers=headers).headers['Content-Length']) ):
-        try:
-            Path(file_name).unlink()
-        except:
-            pass
-        with open(file_name, 'wb') as ff:
-            ff.write(requests.get(url, headers=headers).content)
-            print(name + '.pdf', '(done!)')
-    else:
-        print(file_name + ';  ', 'exists! (skipping)')
-
-
-def video(link, pp, name):
-    link = "https://online.udvash-unmesh.com" + link
-
-    headers = {
-        'authority': 'online.udvash-unmesh.com',
-        'user-agent': user_agent,
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://online.udvash-unmesh.com/Routine',
-        'accept-language': 'en-US,en;q=0.9,ja;q=0.8',
-        'cookie': personal_cookie,
-        'sec-gpc': '1',
-    }
-
-    response = requests.get(link, headers=headers)
-    video_soup = BeautifulSoup(response.text, features='lxml')
-    vlink = video_soup.find('video').source.attrs['src']
-    fname = name + '.mp4'
-    name = pp + '/' + fname
-    headers = {
-        'connection': 'keep-alive',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-        'accept': '*/*',
-        'sec-fetch-site': 'cross-site',
-        'sec-fetch-mode': 'no-cors',
-        'sec-fetch-dest': 'video',
-        'referer': link,
-        'accept-language': 'en-US,en;q=0.9'
-    }
-    if ( not Path(name).is_file() ) or ( Path(name).stat().st_size != int(requests.head(vlink, headers=headers).headers['Content-Length']) ):
-        try:
-            Path(name).unlink()
-        except:
-            pass
-        with open(name, 'wb') as video:
-            video.write(requests.get(vlink, headers=headers).content)
-            print(fname, '(done!)')
-    else:
-        print(name + ';  ', 'exists! (skipping)')
-
-
-
-
+with open("config.json", 'r') as f:
+    pay_load = json.load(f)
 
 
 
@@ -96,48 +18,26 @@ def path(parent, date, month, year):
 
 class udvash():
     def __init__(self):
-        self.load()
-        root = input('Root folder:')
-        with open('load.html', 'r') as load:
-            soup = BeautifulSoup(load, features='lxml')
-            # body = soup.find_all('a', text="Replay Video")
-            test = soup.find_all('div', attrs={'class': 'uu-routine-item routineBox'})
-            for el in test:
-                # print(el.div.attrs['class'])
-                fn = el.find('div', attrs={'class': 'uu-routine-item-head'})
-                name = fn.h2.string.split()[2]
-                ft = el.find('div', attrs={'class': 'uu-routine-item-body'})
-                time = ft.h4.text.split()
-                date = time[3]
-                month = time[4][:-1]
-                year = time[5]
-                pth = []
-                pth.append(path(root, date, month, year))
-                fnt = el.find('div', attrs={'class': 'uu-routine-item-footer'}).contents[1].attrs
-                if 'href' in fnt:
-                    nlink = fnt['href']
-                    cls_notes(nlink, pth[0])
-                fv = el.find('div', attrs={'class': 'uu-routine-item-footer'}).contents
-                if len(fv) >= 4:
-                    vlink = fv[3].attrs['href']
-                    video(vlink, pth[0], name)
+        # root = input('Root folder:')
+        root = ""
+        soup = self.Routine()
+        items = soup.select("div.col-md-4")
+        for item in items:
+            d = self.item(item, root)
+            if len(item.select("a")) > 0:
+                base = "https://online.udvash-unmesh.com"
+                note_link = base + item.select("a")[0]['href']
+                video_link = base + item.select("a")[1]['href']
+                print(note_link, video_link, "********88")
+                self.cls_notes(note_link, d['path'])
+                self.video(video_link, d['path'], d['name'])
 
-    def load(self):
-        headers = {
-            'authority': 'online.udvash-unmesh.com',
-            'accept': '*/*',
-            'dnt': '1',
-            'x-requested-with': 'XMLHttpRequest',
-            'user-agent': user_agent,
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'origin': 'https://online.udvash-unmesh.com',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': 'https://online.udvash-unmesh.com/Routine',
-            'accept-language': 'en-US,en;q=0.9,ja;q=0.8',
-            'cookie': personal_cookie,
-            'sec-gpc': '1',
+    def Routine(self):
+        Headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
+            'Accept': '*/*',
+            'Referer': 'https://online.udvash-unmesh.com/Routine',
+            'Origin': 'https://online.udvash-unmesh.com'
         }
 
         data = {
@@ -148,28 +48,75 @@ class udvash():
         'examPlatform': '2'
         }
 
-        response = requests.post('https://online.udvash-unmesh.com/Routine/LoadRoutineAjax', headers=headers, data=data)
-        # print(response.text)
-        file = 'load.html'
-        try:
-            Path(file).unlink(missing_ok=True)
-        except:
-            pass
-        with open(file, 'w') as load:
-            load.write(response.text)
+        response = requests.post('https://online.udvash-unmesh.com/Routine/LoadRoutineAjax', headers=Headers, data=data)
+        soup = BeautifulSoup(response.text, features='lxml')
+        return soup
+
+    def webpage(self, url):
+        print("Fetching data")
+        Headers = {
+            'User-Agent': pay_load[0]["user_agent"],
+            'Accept': '*/*',
+            'Referer': 'https://online.udvash-unmesh.com/Routine'
+        }
+
+        response = requests.get(url, headers=Headers, cookies=pay_load[1])
+        soup = BeautifulSoup(response.text, features='lxml')
+        return soup
+
+    def item(self, item, root):
+        d = {}
+        d['name'] = "".join(item.select("h2.uu-routine-title")[0].text.split()[2:])
+        time = item.select("h4")[0].text.split()
+        date = time[3]
+        month = time[4][:-1]
+        year = time[5]
+        d['path'] = path(root, date, month, year)
+        return d
+
+    def video(self, link, pp, name):
+        soup = self.webpage(link)
+        video_link = soup.select("#video_1 > source:nth-child(1)")[0]["src"]
+        fname = name + '.mp4'
+        name = pp + '/' + fname
+        Headers = {
+            'Accept': '*/*',
+            'Referer': link,
+            'Connection': 'keep-alive'
+        }
+        if ( not Path(name).is_file() ) or ( Path(name).stat().st_size != int(requests.head(video_link, headers=Headers).headers['Content-Length']) ):
+            try:
+                Path(name).unlink()
+            except:
+                pass
+            with open(name, 'wb') as video:
+                video.write(requests.get(video_link, headers=Headers).content)
+                print(fname, '(done!)')
+        else:
+            print(name + ';  ', 'exists! (skipping)')
+
+    def cls_notes(self, link, pp):
+        soup = self.webpage(link)
+        name = soup.select(".col-md-6")[0].string.split()[2:]
+        url = soup.select(".btn")[0]["href"]
+        file_name = pp + '/' + name + '.pdf'
+        if ( not Path(file_name).is_file() ) or ( Path(file_name).stat().st_size != int(requests.head(url).headers['Content-Length']) ):
+            try:
+                Path(file_name).unlink()
+            except:
+                pass
+            with open(file_name, 'wb') as ff:
+                ff.write(requests.get(url).content)
+                print(name + '.pdf', '(done!)')
+        else:
+            print(file_name + ';  ', 'exists! (skipping)')
 
 
 
-#if __name__ == "__main__":
-#    try:
-#        udvash()
-#        udvash()
-#        print('Please run the script again to make sure everything is fully downloaded')
-#    except:
-#        print('Something went wrong! Please try again')
 
-udvash()
-
-
-# udvash.load('dummy')
-# cls_notes('dummy')
+if __name__ == "__main__":
+   try:
+       udvash()
+       print('Please run the script again to make sure everything is fully downloaded')
+   except:
+       print('Something went wrong! Please try again')
